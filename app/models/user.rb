45fa78451f9ad31d4dbee363ceafee4bd88c7002
Matt Spell <matthew.spell1@gmail.com>
2014-11-21 10:22:49 -0600

    Create Devise User model/signup/signin

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :email
+  validates_presence_of :password, on: :create
+  validates_uniqueness_of :email
+end
